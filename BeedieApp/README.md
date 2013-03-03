3538722608cdcf025e13b2284415ea79f35360f1
William Chen <deviozc@gmail.com>
2013-03-02 21:32:07 -0800

    Initial Project Commit

---
+++ BeedieApp/README.md
@@ -0,0 +1,4 @@
+beedie-android
+==============
+
+Android app for SFU Beedie School of Business
---
+++ BeedieApp/gen/com/beedieapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.beedieapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
